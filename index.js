7e638b2da6c10b6c6cd531eab9c6c9d4acca29df
Larissa Vieira <lv23@nyu.edu>
2022-04-18 22:56:02 -0400

    Index route established and views created for layout and index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const Pattern = require("./models/pattern-model");
 
-app.set("view enigine", "hbs");
+app.set("view engine", "hbs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/patterns", (req, res) => {
   Pattern.find({})
     .then((patterns) => {
-      res.json(patterns);
+      res.render("patterns/index", { patterns });
     })
     .catch(console.error);
 });
