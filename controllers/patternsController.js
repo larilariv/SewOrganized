656d6e59cb87f60493fe6dac9781bd19a4995308
Larissa Vieira <lv23@nyu.edu>
2022-04-23 01:46:57 -0400

    Reworked router.put /:id so it now uses /:name and includes all info from pattern/schema. Tweaked edit view, though some inputs need to be properly configured

--- controllers/patternsController.js
+++ controllers/patternsController.js
@@ -31,14 +31,28 @@ router.get("/:name/edit", (req, res) => {
     .catch(console.error);
 });
 
-router.put("/:id", (req, res) => {
-  const id = req.params.id;
+router.put("/:name", (req, res) => {
   Pattern.findOneAndUpdate(
-    { _id: id },
+    { name: req.params.name },
     {
-      name: req.body.name,
       brand: req.body.brand,
+      coverImage: req.body.coverImage,
+      lineArtImage: req.body.lineArtImage,
+      sizeChartImage: req.body.sizeChartImage,
+      link: req.body.link,
       categories: req.body.categories,
+      description: req.body.description,
+      difficulty: req.body.difficulty,
+      fabricType: req.body.fabricType,
+      fabricChoices: req.body.fabricChoices,
+      format: req.body.format,
+      digitalPrinted: req.body.digitalPrinted,
+      inPrint: req.body.inPrint,
+      quantity: req.body.quantity,
+      rating: req.body.rating,
+      letterSize: req.body.letterSize,
+      numSize: req.body.numSize,
+      tags: req.body.tags,
     },
     { new: true }
   )
